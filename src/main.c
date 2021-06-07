897c37ea7520ca1b6c4064361acc36b4d735f6b6
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-06-07 17:27:19 +0200

    commit

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include "../inc/myhead.h"
+#include <conio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "../inc/map.h"
+int score;
+bool gameover;
+int fruitx,fruity;
+int snakex, snakey;
+
 int main()
 {
-    add(4, 6);
-
-    /*This calls add function written in myhead.h  
-      and therefore no compilation error.*/
-    multiply(5, 5);
-
-    // Same for the multiply function in myhead.h
-    printf("BYE!See you Soon");
-    return 0;
+  drawBoundaries();
+  return 0;
 }
