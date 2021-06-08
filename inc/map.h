1fb3ee83f366f8294588a42609dece84189d4915
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-06-08 11:50:02 +0200

    commit

--- inc/map.h
+++ inc/map.h
@@ -38,3 +38,4 @@ void drawMap()
         printf("\n");
     }
 }
+
--- src/main.c
+++ src/main.c
@@ -10,11 +10,15 @@ int snakePositions[400][1];
 int sizeOfSP ;
 char pressedKey;
 
+void showSnakePostions();
+
 void initalizeSnake();
 void initalizeApple();
 void controlHandler(char key);
 
 void setSnakePosition(int,int);
+
+void drawSnake();
 
 void up();
 void down();
@@ -25,12 +29,17 @@ int main()
 {
   initializeBoundaries();
   initalizeSnake();
+  showSnakePostions();
   initalizeApple();
   drawMap();
   while (gameover == false){
     scanf(" %c", &pressedKey);
     if (pressedKey == 'r'){
-      up();
+      showSnakePostions();
+      setSnakePosition(10, 4);
+      showSnakePostions();
+      drawSnake();
+      drawMap();
       gameover = true;
     }
     controlHandler(pressedKey);
@@ -48,22 +57,14 @@ void initalizeSnake(){
 }
 
 void setSnakePosition(int newX, int newY){
-  sizeOfSP = sizeof(snakePositions) / sizeof(int);
+  sizeOfSP = (sizeof(snakePositions) / sizeof(int))/2;
   for (int i = 0; i <= sizeOfSP; i++){
-    snakePositions[i][0] = snakePositions[i - 1][0];
-    snakePositions[i][1] = snakePositions[i - 1][1];
+    snakePositions[i][0] = snakePositions[i + 1][0];
+    snakePositions[i][1] = snakePositions[i + 1][1];
   }
-  sizeOfSP = sizeof(snakePositions) / sizeof(int);
+  sizeOfSP = (sizeof(snakePositions) / sizeof(int))/2;
   snakePositions[sizeOfSP + 1][0] = newX;
   snakePositions[sizeOfSP + 1][1] = newY;
-}
-
-void drawSnake(){
-sizeOfSP = sizeof(snakePositions) / sizeof(int);
-int i, j;
-for (i = 0; i < sizeOfSP; i++){
-  setPixelValue(snakePositions[i][0],snakePositions[i][1],'*');
-}
 }
 
 
@@ -73,8 +74,20 @@ void initalizeApple(){
   setPixelValue(fruitx, fruity, '@');
 }
 
+
+
+void drawSnake()
+{
+  sizeOfSP = sizeof(snakePositions) / sizeof(int);
+  int i, j;
+  for (i = 0; i <= sizeOfSP; i++)
+  {
+    setPixelValue(snakePositions[i][0], snakePositions[i][1], '*');
+  }
+}
+
 void controlHandler(char key){
-  void(* snakeMovements[])() = {up,down,left,right};
+  void(*snakeMovements[])() = {up,down,left,right};
 }
 
 void up(){
@@ -100,4 +113,21 @@ void right(){
   setSnakePosition(snakePositions[sizeOfSP + 1][0], snakePositions[sizeOfSP][1]);
   drawSnake();
   drawMap();
+}
+
+void showSnakePostions()
+{
+  printf("snake's positions\n");
+  int i, j;
+  for (i = 0; i <= 3; i++)
+  {
+    for (j = 0; j <= 1; j++)
+    {
+      printf("snakePostions[%d][%d] ->",i,j);
+      printf("%d\n", snakePositions[i][j]);
+    }
+    printf("\n");
+  }
+ 
+  printf("\n%d\n", (sizeof(snakePositions) / sizeof(int)) / 2);
 }
